1bb51fa904e60f4e68a9a8e34fd357a5b6f2e855
Connor Henderson <connor.henderson@talkiatry.com>
2022-12-07 23:31:51 -0500

    psych using prisma

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,14 @@
 import NextAuth from "next-auth";
-import SequelizeAdapter from "@next-auth/sequelize-adapter";
-import db from "../../../db/models";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prismadb";
 
-db.sequelize.sync();
-
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
 export default NextAuth({
-  // https://next-auth.js.org/providers/overview
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  adapter: SequelizeAdapter(db.sequelize),
 });
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import db from "../../db/models";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    await db.sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-
     res.status(200).json({ name: "John Doe" });
   } catch (error) {
     console.error("Unable to connect to the database:", error);
