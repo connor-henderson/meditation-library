aa9cde6a3aae706ff914cce64c0f612d4f69bc80
Connor Henderson <connor.henderson@talkiatry.com>
2022-12-14 09:46:03 -0500

    added mui, seeder, and initial test pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
-import Head from "next/head";
 
 const theme = createTheme({
   palette: {
@@ -12,22 +11,11 @@ const theme = createTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Mindfulnet</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        {/* <link rel="icon" href="favicon.ico" /> */}
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
           <Component {...pageProps} />
         </SessionProvider>
       </ThemeProvider>
-    </>
   );
 }
--- src/pages/home.tsx
+++
@@ -1,8 +0,0 @@
-import { Button, Grid } from "@mui/material";
-import { signOut, useSession } from "next-auth/react";
-
-const Home = () => {
-  return <Grid>Test</Grid>;
-};
-
-export default Home;
